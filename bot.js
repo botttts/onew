e104a21c0ebe85a50b44653acc668e591292404b
botttts <45695807+botttts@users.noreply.github.com>
2018-12-07 21:24:48 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,9 +1,6 @@
 cconst Discord = require('discord.js');
 const client = new Discord.Client();
  
-client.on('ready', () => {
-  console.log(`Logged in as ${client.user.tag}!`);
-   client.user.setActivity("F*ck love..",{type: 'WATCHING'})
   console.log('')
   console.log('')
   console.log('╔[═════════════════════════════════════════════════════════════════]╗')
