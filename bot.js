b13beedddf7189dea9f56241bc734ca4645f7b64
botttts <45695807+botttts@users.noreply.github.com>
2018-12-07 20:53:52 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -6,11 +6,20 @@ client.on('ready', () => {
 });
 
 client.on('message', msg => {
-  if (msg.content === 'ping') {
-    msg.reply('Pong!');
+  if (msg.content === 'هنا') {
+    msg.reply('ماستر الملك الكبير');
   }
 });
 
+client.on('ready', () => {
+  console.log(`Logged in as ${client.user.tag}!`);
+});
+
+client.on('message', msg => {
+  if (msg.content === 'هاي') {
+    msg.reply('هلا فيك حب');
+  }
+});
 
 client.login(process.env.BOT_TOKEN);
 
